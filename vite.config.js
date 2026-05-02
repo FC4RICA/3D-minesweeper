2256c79da68cfdd6932c7372371c9be15a24d7cb
FC4RICA <kantinan.fw@gmail.com>
2026-05-02 20:36:30 +0700

    Refactor onCellClick logic

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true,      // expose to 0.0.0.0
+    port: 5173,
+  },
 })
